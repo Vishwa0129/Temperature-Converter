4682599434f7d7088d6bc41440b310b74d1df6c9
Vishwa0129 <141856687+Vishwa0129@users.noreply.github.com>
2023-08-25 14:32:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 
-function convert(){
+function convert() {
     var valNum = parseFloat(document.getElementById("temperature").value);
+    var scale = document.getElementById("scale").value;
+    var result = document.getElementById("result");
 
-    if (document.getElementById("scale").value == "fahrenheit"){
-        document.getElementById("result").innerHTML = ((valNum-32)*(5/9)).toFixed(2) + " Celsius";
+    if (scale === "fahrenheit") {
+        var celsius = ((valNum - 32) * (5 / 9)).toFixed(2);
+        var kelvin = (parseFloat(celsius) + 273.15).toFixed(2);
+        result.innerHTML = celsius + " Celsius<br>" + kelvin + " Kelvin";
+    } else if (scale === "celsius") {
+        var fahrenheit = ((valNum * 9 / 5) + 32).toFixed(2);
+        var kelvin = (parseFloat(valNum) + 273.15).toFixed(2);
+        result.innerHTML = fahrenheit + " Fahrenheit<br>" + kelvin + " Kelvin";
+    } else if (scale === "kelvin") {
+        var celsius = (valNum - 273.15).toFixed(2);
+        var fahrenheit = ((celsius * 9 / 5) + 32).toFixed(2);
+        result.innerHTML = celsius + " Celsius<br>" + fahrenheit + " Fahrenheit";
     } else {
-        document.getElementById("result").innerHTML = ((valNum*(9/5))+32).toFixed(2) + " Fahrenheit";
+        result.innerHTML = "Invalid scale";
     }
 }
